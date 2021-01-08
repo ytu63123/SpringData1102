2a226995ec534508b9da187e0bff42a4209dc2c2
student <student@192.168.1.92>
2021-01-08 21:48:51 +0800

    SpringData

--- src/main/java/com/spring/mvc/psi/controller/PSIController.java
+++ src/main/java/com/spring/mvc/psi/controller/PSIController.java
@@ -1,9 +1,18 @@
 package com.spring.mvc.psi.controller;
 
 import com.spring.mvc.psi.entities.Product;
+import com.spring.mvc.psi.entities.Purchase;
+import com.spring.mvc.psi.entities.Sales;
+import com.spring.mvc.psi.repository.Inventory2Repository;
+import com.spring.mvc.psi.repository.InventoryRepository;
 import com.spring.mvc.psi.repository.ProductRepository;
+import com.spring.mvc.psi.repository.PurchaseRepository;
+import com.spring.mvc.psi.repository.SalesRepository;
 import java.util.Optional;
+import org.springframework.util.MultiValueMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,6 +21,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -20,7 +30,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class PSIController {
 
     @Autowired
+    private InventoryRepository inventoryRepository;
+    @Autowired
+    private Inventory2Repository inventory2Repository;
+    @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private PurchaseRepository purchaseRepository;
+
+    @Autowired
+    private SalesRepository salesRepository;
 
     @GetMapping(value = {"/product", "/product/{id}", "/product/{name}/{id}"})
     public String readProduct(Model model,
@@ -39,7 +59,7 @@ public class PSIController {
         model.addAttribute("_method", _method);
         model.addAttribute("product", product);
         model.addAttribute("products", productRepository.findAll());
-        model.addAttribute("deleteError", deleteError.isPresent()?"刪除失敗，已有商品購買紀錄，不能刪除":"");
+        model.addAttribute("deleteError", deleteError.isPresent() ? "刪除失敗，已有商品購買紀錄，不能刪除" : "");
         return "psi/product";
     }
 
@@ -58,12 +78,67 @@ public class PSIController {
     @DeleteMapping(value = {"/product"})
     public String deleteProduct(@ModelAttribute("product") Product product) {
         try {
-             productRepository.delete(product.getId());
+            productRepository.delete(product.getId());
         } catch (Exception e) {
             return "redirect: ../product?deleteError=true";
         }
-       return "redirect: ../product";
-        
+        return "redirect: ../product";
+
+    }
+//讀取庫存
+
+    @GetMapping(value = {"/inventory"})
+    public String readInventory(Model model) {
+        model.addAttribute("inventories", inventoryRepository.findAll());
+        model.addAttribute("inventories2", inventory2Repository.findAll());
+
+        return "psi/inventory";
+    }
+//讀取進貨
+
+    @GetMapping(value = {"/purchase"})
+    public String readPurchase(Model model) {
+        model.addAttribute("purchases", purchaseRepository.findAll());
+        model.addAttribute("inventories2", inventory2Repository.findAll());
+
+        return "psi/purchase";
+    }
+
+    @PostMapping(value = {"/purchase"},
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String createPurchase(@RequestBody MultiValueMap<String, String> map) {
+        Integer pid = Integer.parseInt(map.getFirst("pid"));
+        Integer quantity = Integer.parseInt(map.getFirst("quantity"));
+        Integer price = Integer.parseInt(map.getFirst("price"));
+        Purchase purchase = new Purchase();
+        purchase.setProduct(productRepository.findOne(pid));
+        purchase.setPrice(price);
+        purchase.setQuantity(quantity);
+        purchaseRepository.saveAndFlush(purchase);
+        return "redirect: ../psi/purchase";
+    }
+
+//讀取銷貨
+    @GetMapping(value = {"/sales"})
+    public String readSales(Model model) {
+        model.addAttribute("sales", salesRepository.findAll());
+        model.addAttribute("inventories2", inventory2Repository.findAll());
+
+        return "psi/sales";
+    }
+
+    @PostMapping(value = {"/sales"},
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String createSales(@RequestBody MultiValueMap<String, String> map) {
+        Integer pid = Integer.parseInt(map.getFirst("pid"));
+        Integer quantity = Integer.parseInt(map.getFirst("quantity"));
+        Integer price = Integer.parseInt(map.getFirst("price"));
+        Sales sales = new Sales();
+        sales.setProduct(productRepository.findOne(pid));
+        sales.setPrice(price);
+        sales.setQuantity(quantity);
+        salesRepository.saveAndFlush(sales);
+        return "redirect: ../psi/sales";
     }
 
 }
